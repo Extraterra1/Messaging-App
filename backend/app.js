aab02150cb46b69f882535b7c268e82041bc8a58
Carlos Pires <extraterra1@gmail.com>
2024-02-06 10:55:30 +0000

    dev: error handling + 404

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose = require('mongoose');
+const { default: mongoose } = require('mongoose');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const errorHandler = require('./middleware/errorHandler');
+const handle404 = require('./middleware/handle404');
 
 require('dotenv').config();
 const app = express();
@@ -14,6 +17,7 @@ mongoose.connect(process.env.DB_URL);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error'));
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,5 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(errorHandler);
+app.use(handle404);
 
 module.exports = app;
