8a0c9c42cf4107692fdde3223e3bb14bca778ea5
Carlos Pires <extraterra1@gmail.com>
2024-02-07 15:43:54 +0000

    dev: passport jwt verify

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,7 +1,20 @@
 const { ExtractJwt, Strategy: JwtStrategy } = require('passport-jwt');
 const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
 
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_SECRET
 };
+
+const strategy = new JwtStrategy(options, async (payload, done) => {
+  try {
+    const user = await User.findById(payload.sub);
+
+    if (!user) return done(null, false);
+
+    return done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
