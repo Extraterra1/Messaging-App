d0321a0ffafdf510dbed77e4c6503dba223fa5ee
Carlos Pires <extraterra1@gmail.com>
2024-02-09 12:39:06 +0000

    feat: create chatroom

--- backend/controllers/chatroomController.js
+++ backend/controllers/chatroomController.js
@@ -1,22 +1,32 @@
 const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 const { isValidObjectId } = require('mongoose');
+const Chatroom = require('../models/chatroomModel');
 
 exports.create = [
   body('participants', 'You need to pass the members of the chatrooms')
-    .isArray()
-    .isLength({ min: 2 })
+    .notEmpty()
+    .isArray({ min: 2 })
     .withMessage('There must be at least 2 members')
     .custom((value) => value.length === new Set(value).size)
     .withMessage('All participants must be unique'),
   body('participants.*')
     .custom((value) => isValidObjectId(value))
     .withMessage('Invalid User ID'),
+  body('title', 'Title cannot be longer than 15 characters').optional().isLength({ max: 15 }),
 
   asyncHandler(async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(401).json({ err: errors.array(), type: 'bodyValidation' });
 
-    return res.json({ participants: req.body.participants });
+    const newChatroom = new Chatroom({
+      participants: req.body.participants
+    });
+
+    if (req.body.title) newChatroom.title = req.body.title;
+
+    await newChatroom.save();
+
+    return res.json({ newChatroom });
   })
 ];
--- backend/models/chatroomModel.js
+++ backend/models/chatroomModel.js
@@ -1,6 +1,10 @@
 const { default: mongoose, Schema } = require('mongoose');
 
 const chatroomModel = new Schema({
+  title: {
+    type: String,
+    trim: true
+  },
   participants: [
     {
       type: mongoose.Types.ObjectId,
@@ -14,7 +18,11 @@ const chatroomModel = new Schema({
       required: true,
       ref: 'Comment'
     }
-  ]
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Chatroom', chatroomModel);
