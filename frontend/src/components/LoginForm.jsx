ada5fb7cd060146a7b149bf9fe183eda417f6987
Carlos Pires <extraterra1@gmail.com>
2024-02-19 15:21:49 +0000

    feat: log in

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,11 +1,12 @@
 import { Formik, Form, useField } from 'formik';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-// import useAxios from 'axios-hooks';
+import useAxios from 'axios-hooks';
 import * as Yup from 'yup';
-// import { useSignIn, useIsAuthenticated } from 'react-auth-kit';
 import { Navigate, Link, useLocation } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
+import useSignIn from 'react-auth-kit/hooks/useSignIn';
+import useIsAuthenticated from 'react-auth-kit/hooks/useIsAuthenticated';
 
 const FormWrapper = styled.div`
   display: grid;
@@ -103,52 +104,54 @@ Input.propTypes = {
 };
 
 const LoginForm = () => {
-  //   const signIn = useSignIn();
-  //   const isAuthenticated = useIsAuthenticated();
-  //   const [{ data }, executeLogin] = useAxios({ url: `${import.meta.env.VITE_API_URL}/login`, method: 'POST' }, { manual: true });
-  //   const location = useLocation();
+  const signIn = useSignIn();
+  const isAuthenticated = useIsAuthenticated();
+  const [{ data }, executeLogin] = useAxios({ url: `${import.meta.env.VITE_API_URL}/login`, method: 'POST' }, { manual: true });
+  const location = useLocation();
 
-  //   if (data) {
-  //     signIn({
-  //       token: data.token,
-  //       tokenType: 'Bearer',
-  //       authState: data.user,
-  //       expiresIn: 60
-  //     });
-  //   }
+  if (data) {
+    signIn({
+      auth: {
+        token: data.token,
+        type: 'Bearer'
+      },
+      userState: data.user
+    });
+  }
 
-  //   const handleSubmit = async (values, { setSubmitting }) => {
-  //     try {
-  //       toast.promise(
-  //         executeLogin({ data: { username: values.username, password: values.password } }),
-  //         {
-  //           loading: 'Logging in...',
-  //           success: 'Logged In! Redirecting...',
-  //           error: (err) => (err.response ? 'Wrong Username/Password' : 'Something went wrong')
-  //         },
-  //         {
-  //           style: {
-  //             marginTop: '3rem',
-  //             fontSize: '1.5rem'
-  //           },
-  //           success: {
-  //             duration: 3000
-  //           },
-  //           error: {
-  //             duration: 5000
-  //           },
-  //           id: 'loginAttempt'
-  //         }
-  //       );
-  //       setSubmitting(false);
-  //     } catch (err) {
-  //       console.log(err.message);
-  //     }
-  //   };
+  const handleSubmit = async (values, { setSubmitting }) => {
+    try {
+      toast.promise(
+        executeLogin({ data: { username: values.username, password: values.password } }),
+        {
+          loading: 'Logging in...',
+          success: 'Logged In! Redirecting...',
+          error: (err) => (err.response ? 'Wrong Username/Password' : 'Something went wrong')
+        },
+        {
+          style: {
+            marginTop: '3rem',
+            fontSize: '1.5rem'
+          },
+          success: {
+            duration: 3000
+          },
+          error: {
+            duration: 5000
+          },
+          id: 'loginAttempt'
+        }
+      );
+      setSubmitting(false);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
   return (
     <>
-      {/* {isAuthenticated() && <Navigate to={location.state ? location.state.pathname : '/'} />} */}
+      {isAuthenticated() ? <h1>Authed</h1> : null}
+      {isAuthenticated() && <Navigate to={location.state ? location.state.pathname : '/'} />}
       <Formik
         initialValues={{
           username: '',
@@ -158,7 +161,7 @@ const LoginForm = () => {
           username: Yup.string().required('Required'),
           password: Yup.string().required('Required')
         })}
-        onSubmit={() => toast.success('submitted')}
+        onSubmit={handleSubmit}
       >
         <FormWrapper>
           <Form style={formCSS}>
