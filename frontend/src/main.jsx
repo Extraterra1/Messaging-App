176570e5a2b9539248e5c15fe81152ba1021928e
Carlos Pires <extraterra1@gmail.com>
2024-02-19 15:06:33 +0000

    dev: react auth kit setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import WebFont from 'webfontloader';
+import AuthProvider from 'react-auth-kit/AuthProvider';
+import createStore from 'react-auth-kit/createStore';
+
 import './index.css';
 
 import Router from '../Router';
+
+const store = createStore({
+  authName: '_auth',
+  authType: 'cookie',
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === 'https:'
+});
 
 WebFont.load({
   google: {
@@ -13,6 +23,8 @@ WebFont.load({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router />
+    <AuthProvider store={store}>
+      <Router />
+    </AuthProvider>
   </React.StrictMode>
 );
