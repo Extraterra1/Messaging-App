1bccb14493ce637153310515ac7307c6b1ad3d07
Carlos Pires <extraterra1@gmail.com>
2024-02-22 16:31:28 +0000

    dev: register routing

--- frontend/Router.jsx
+++ frontend/Router.jsx
@@ -3,6 +3,7 @@ import RequireAuth from '@auth-kit/react-router/RequireAuth';
 
 import Landing from './src/views/Landing';
 import Login from './src/views/Login';
+import Register from './src/views/Register.jsx';
 
 const Router = () => {
   const router = createBrowserRouter([
@@ -17,6 +18,10 @@ const Router = () => {
     {
       path: '/login',
       element: <Login />
+    },
+    {
+      path: '/register',
+      element: <Register />
     }
   ]);
   return <RouterProvider router={router} />;
