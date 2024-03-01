40fabb7be0fe60272f4fdf5bbf150419a7a68f6d
Carlos Pires <extraterra1@gmail.com>
2024-03-01 15:49:46 +0000

    feat: send friend request

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -1,19 +1,42 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
+import useAuthHeader from 'react-auth-kit/hooks/useAuthHeader';
 import { Icon } from '@iconify/react';
+import useAxios from 'axios-hooks';
+import toast from 'react-hot-toast';
+import { BeatLoader } from 'react-spinners';
+import { useState } from 'react';
 
 import CircleLetter from './CircleLetter';
 
 const FriendCard = ({ user }) => {
   const auth = useAuthUser();
   const alreadyFriends = user.friends.some((e) => e.user === auth._id);
+  const authHeader = useAuthHeader();
+
+  const [{ loading, data }, sendFriendRequest] = useAxios(
+    { url: `${import.meta.env.VITE_API_URL}/friendRequests`, method: 'POST', headers: { Authorization: authHeader } },
+    { manual: true }
+  );
+
+  const handleClick = () => {
+    if (alreadyFriends) return;
+    toast.promise(sendFriendRequest({ data: { recipient: user._id } }), {
+      loading: 'Sending Friend Request...',
+      success: 'Friend Request Sent!',
+      error: 'Something Went Wrong...'
+    });
+  };
 
   return (
     <Container>
       <CircleLetter>{user.username.at(0).toUpperCase()}</CircleLetter>
       <span className="username">{user.username}</span>
-      <Icon className={`add-friend-icon icon ${alreadyFriends && 'friends'}`} icon="ph:user-plus-fill" />
+      {!loading ? (
+        <Icon onClick={handleClick} className={`add-friend-icon icon ${alreadyFriends && 'friends'}`} icon={data ? 'ph:check-bold' : 'ph:user-plus-fill'} />
+      ) : null}
+      <BeatLoader loading={loading} color="var(--light)" size={5} />
     </Container>
   );
 };
